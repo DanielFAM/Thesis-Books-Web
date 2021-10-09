ffe79cece2ac1dc1394eceeed9d770f75a2e1a75
Daniel Andrade <danfe_1999@outlook.com>
2021-10-08 23:01:04 -0500

    Creation of validation token class

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,16 +3,22 @@ const User = require('../models/users');
 const jwt = require('jsonwebtoken');
 
 authCtrl.signUp = async (req, res) => {
-    const user = new User (req.body);
-    user.password = await user.encryptPassword(user.password);
-    await user.save();
+    const { username, email, password } = req.body;
+
+    const newUser = new User({
+        username,
+        email,
+        password: await User.encryptPassword(password)
+    });
+
+    const savedUser = await newUser.save();
     //Creación del token de autenticación que vence en 1 dia
     //Pide el id del objeto mas una cadena secreta
     //finalmente pide el tiempo en segundos en el que expirara
-    const token = jwt.sign({id: user._id}, process.env.SECRET, {
-        expiresIn: 60 * 60 * 24
-    })
-    res.json({auth: true, token})
+    const token = jwt.sign({id: savedUser._id}, process.env.SECRET,{
+        expiresIn: 86400 //24 horas
+    });  
+    res.status(200).json({token})
 };
 
 authCtrl.me = async (req,res) => {
@@ -27,18 +33,40 @@ authCtrl.me = async (req,res) => {
 
 authCtrl.signIn = async (req, res) => {
     const { email, password } = req.body;
-    const user = await User.findOne({email: email});
-    if(!user){
-        res.status(404).send("The email doesn't exists");
-    }
-    const validPassword = await user.validatePassword(password);
-    if(!validPassword) {
-        return res.status(401).json({auth: false, token: null})
-    }
-    const token = jwt.sign({id: user._id}, process.env.SECRET, {
+
+    const userFound = await User.findOne({email: email});
+
+    if(!userFound) res.status(400).json({message: 'user not found'});
+
+    const validPassword = await userFound.validatePassword(password);
+
+    if(!validPassword) return res.status(401).json({token: null, message: 'Invalid password'});
+    
+    const token = jwt.sign({id: userFound._id}, process.env.SECRET, {
         expiresIn: 60 * 60 * 24
     });
-    res.json({auth:true, token});
+    
+    res.json({token});
+};
+
+authCtrl.verifyToken = async (req, res) => {
+    try {
+        //verifica que exista una cabecera de la petición que tenga el token
+        const token = req.headers ['x-access-token'];
+        //si no existe envia mensaje de error
+        if(!token) return res.status(403).json({message:'No token provided'});
+        //Si existe decodifica el token y reconoce el _id del usuario
+        const decoded = jwt.verify(token, process.env.SECRET);
+        req.userId = decoded.id;
+        //verifica que el id del usuario pertenezca a la BD
+        const user = User.findById(decoded.id);
+        //si no existe usuario, envia error
+        if(!user) return res.status(404).json({message:"no user found"});
+        res.json({message:'Token is valid'});
+    
+    } catch (error) {
+        return res.status(401).json({message: 'unauthorized'});
+    }
 };
 
 module.exports = authCtrl;
--- backend/src/middlewares/verifyToken.js
+++ backend/src/middlewares/verifyToken.js
@@ -1,19 +1,24 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/users');
 
 function verifyToken (req, res, next) {
-    //verifica que exista una cabecera de la petición que tenga el token
-    const token = req.headers ['x-access-token'];
-    if(!token){
+    try {
+        //verifica que exista una cabecera de la petición que tenga el token
+        const token = req.headers ['x-access-token'];
         //si no existe envia mensaje de error
-        return res.status(401).json({
-            auth:false,
-            message:'No token provided'
-        });
+        if(!token) return res.status(403).json({message:'No token provided'});
+        //Si existe decodifica el token y reconoce el _id del usuario
+        const decoded = jwt.verify(token, process.env.SECRET);
+        req.userId = decoded.id;
+        //verifica que el id del usuario pertenezca a la BD
+        const user = User.findById(req.userId, {password:0});
+        //si no existe usuario, envia error
+        if(!user) return res.status(404).json({message:"no user found"});
+
+        next();
+    } catch (error) {
+        return res.status(401).json({message: 'unauthorized'});
     }
-    //Si existe decodifica el token y reconoce el _id del usuario
-    const decoded = jwt.verify(token, process.env.SECRET);
-    req.userId = decoded.id;
-    next();
 }
 
 module.exports = verifyToken;
